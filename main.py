9d51d56036ca95d0ef909d8ce8dc3900007e7597
zahrizhalali29 <zzahrizhal.ali@gmail.com>
2021-10-13 13:43:33 +0800

    v.3

--- main.py
+++ main.py
@@ -3,20 +3,19 @@ import requests
 
 app = Flask(__name__)
 
+response = requests.get("https://api.npoint.io/b45a3d3518ef5f96d0e6")
+data = response.json()
 
 @app.route('/')
 def home():
-    response = requests.get("https://api.npoint.io/b45a3d3518ef5f96d0e6")
-    data = response.json()
+
     return render_template("index.html", posts=data)
 
 
 @app.route("/post/<int:blog_id>")
 def get_post(blog_id):
-    post_response = requests.get("https://api.npoint.io/b45a3d3518ef5f96d0e6")
-    the_post = post_response.json()
-    curr_post = {}
-    return render_template("post.html", post=the_post, blog_id=blog_id)
+
+    return render_template("post.html", post=data, blog_id=blog_id)
 
 
 if __name__ == "__main__":
