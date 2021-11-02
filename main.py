7d78c04994519c6ed8312ff086e7efa7ad189a71
zahrizhalali29 <zzahrizhal.ali@gmail.com>
2021-11-02 23:50:46 +0800

    Add authentication

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, flash, request, abort
+from functools import wraps
 from flask_bootstrap import Bootstrap
+from flask_ckeditor import CKEditor
+from datetime import date
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, URL
-from flask_ckeditor import CKEditor, CKEditorField
-import requests
-import smtplib
+from sqlalchemy.orm import relationship
+from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+from flask_gravatar import Gravatar
+from forms import CreatePostForm, RegisterForm, LoginForm
 import datetime
+import smtplib
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
@@ -19,10 +23,29 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+
 # response = requests.get("https://api.npoint.io/b45a3d3518ef5f96d0e6")
 # data = response.json()
 OWN_EMAIL = "blabla@gmail.com"
 OWN_PASSWORD = "blablabla@123"
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+def admin_only(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        # If id is not 1 then return abort with 403 error
+        if current_user.id != 1:
+            return abort(403)
+        # Otherwise continue with the route function
+        return f(*args, **kwargs)
+    return decorated_function
 
 
 # CONFIGURE TABLE
@@ -35,24 +58,68 @@ class BlogPost(db.Model):
     author = db.Column(db.String(250), nullable=False)
     img_url = db.Column(db.String(250), nullable=False)
 
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(1000))
 # db.create_all()
-
-
-# WTForm
-class CreatePostForm(FlaskForm):
-    title = StringField("Blog Post Title", validators=[DataRequired()])
-    subtitle = StringField("Subtitle", validators=[DataRequired()])
-    author = StringField("Your Name", validators=[DataRequired()])
-    img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
-    # body = StringField("Blog Content", validators=[DataRequired()])
-    body = CKEditorField("Blog Content", validators=[DataRequired()])
-    submit = SubmitField("Submit Post")
 
 
 @app.route('/')
 def home():
     data = db.session.query(BlogPost).all()
     return render_template("index.html", all_post=data)
+
+
+# AUTHENTICATION
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    register_form = RegisterForm()
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    if request.method == 'POST':
+        if User.query.filter_by(email=register_form.email.data).first():
+            flash('User already registered.')
+            return redirect(url_for('login'))
+        password = request.form['password']
+        hashed_password = generate_password_hash(password, method='pbkdf2:sha256', salt_length=8)
+        new_user = User(email=register_form.email.data, password=hashed_password, name=register_form.name.data)
+        db.session.add(new_user)
+        db.session.commit()
+        login_user(new_user)
+        return redirect(url_for('home'))
+    return render_template("register.html", form=register_form)
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    login_form = LoginForm()
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    if request.method == 'POST':
+        get_user = User.query.filter_by(email=login_form.email.data).first()
+        if get_user:
+            user_pass = get_user.password
+            check = check_password_hash(user_pass, login_form.password.data)
+            if check:
+                user_pass = ""
+                login_user(get_user)
+                return redirect(url_for("home"))
+            else:
+                flash("Password wrong.")
+                return redirect(url_for("login"))
+        else:
+            flash("User not found. Please register first.")
+            return redirect(url_for("login"))
+    return render_template("login.html", form=login_form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
 
 
 @app.route("/blog/<int:blog_id>")
@@ -63,6 +130,8 @@ def get_blog(blog_id):
 
 
 @app.route('/edit/<int:blog_id>', methods=['GET', 'POST'])
+@login_required
+@admin_only
 def edit_post(blog_id):
     data = BlogPost.query.get(blog_id)
     post_form = CreatePostForm(
@@ -84,6 +153,8 @@ def edit_post(blog_id):
 
 
 @app.route('/new-post', methods=['POST', 'GET'])
+@login_required
+@admin_only
 def new_post():
     post_form = CreatePostForm()
     title = post_form.title.data
@@ -123,6 +194,8 @@ def send_email(name, email, phone, message):
 
 
 @app.route('/delete/<int:blog_id>', methods=['POST', 'GET'])
+@login_required
+@admin_only
 def delete_post(blog_id):
     book_to_delete = BlogPost.query.get(blog_id)
     db.session.delete(book_to_delete)
@@ -136,7 +209,7 @@ if __name__ == "__main__":
 # import bleach
 
 
-## strips invalid tags/attributes
+# strips invalid tags/attributes
 # def strip_invalid_html(content):
 #     allowed_tags = ['a', 'abbr', 'acronym', 'address', 'b', 'br', 'div', 'dl', 'dt',
 #                     'em', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'hr', 'i', 'img',
